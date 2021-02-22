be8222e684bf3dd332ae31bad62fb238d3482b48
Pete Mariani <pcmariani@gmail.com>
2021-02-22 17:38:27 +0000

    exit if .dotrepo exists

--- dotfiles-bare-git-repo.sh
+++ dotfiles-bare-git-repo.sh
@@ -6,15 +6,19 @@ function dot {
 
 echo ".dotrepo" >> .gitignore
 
-git clone --bare https://github.com/pcmariani/dotfiles $HOME/.dotrepo
+if [ ! -d $HOME/.dotrepo ]; then
+    git clone --bare https://github.com/pcmariani/dotfiles $HOME/.dotrepo
+else
+    echo "The directory $HOME/.dotrepo already exists. Aborting."
+    exit 1
+fi
 
-mkdir -p .dotrepo-backup
-dot checkout
-
+dot checkout 2>/dev/null
 if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
+    echo "Checked out config.";
+else
     echo "Backing up pre-existing dot files.";
+    mkdir -p .dotrepo-backup
     dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotrepo-backup/{}
 fi;
 

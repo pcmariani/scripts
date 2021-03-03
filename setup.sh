dfaa9c22c78d7baf95f0355345d7d037f7a0a295
Pete Mariani <pcmariani@gmail.com>
2021-03-03 01:12:29 +0000

    complete-ish; comment boneyard

--- setup.sh
+++ setup.sh
@@ -1,78 +1,234 @@
 #!/bin/sh
 
-logging=
-verbose=
+#   <~-- - -~<o>~- - --~>
+#
+#   Debian WSL setup
+#
+#   <~-- - -~<o>~- - --~>
 
-# Script core {{{
-    colorscheme() {
-        reset="$(tput sgr0)$(tput setaf 253)" #grey
-        title="$(tput setaf 214)" #orange
-        question="$(tput setaf 81)" #darker cyan
-        answer="$(tput bold)$(tput setaf 255)" #white
-        success="$(tput setaf 2)" #green
-        failed="$(tput setaf 1)" #red
-        stderr="$(tput setaf 175)" #white
+
+eecho() {
+    echo "${green}$@${reset}"
     }
 
-    log_init() {
-        logfile=./install-debian-testing.log
-        [ "$logging" ] && log "INFO Starting install..." > $logfile
+colors() {
+    green="$(tput setaf 2)"
+    reset="$(tput sgr0)"
     }
 
-    log() {
-        [ "$logging" ] && printf "$(date +"%T") $@\n" >> $logfile
+debian_prereqs() {
+    umask 022
+    echo "Set root password:"; passwd
+    for package in readline-common dialog apt-utils build-essential \
+        sudo wget make cmake file git ; do
+        eecho "apt install $package..."
+        apt install -y "$package"
+    done
     }
 
-    h1() {
-        printf "${title}$@${reset}\n\n"
+fix_locale() {
+    eecho "Fixing locale..."
+    apt purge locales -y
+    apt install locales -y
+    echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+    dpkg-reconfigure --frontend=noninteractive locales
+    update-locale LANG=en_US.UTF-8
     }
 
-    p() {
-        printf "${reset}$@\n"
+create_user() {
+    # $username and $defaultShell env variables set by wsl setup script
+    eecho "Creating user $username..."
+    useradd --create-home --user-group --shell /bin/$defaultShell "$username"
+    #echo "$username:$pass1" | chpasswd
+    #unset pass1 pass2
+    eecho "Adding $username to sudo group..."
+    usermod -aG sudo "$username"
+    eecho "Adding $username to sudoers..."
+    echo "$username  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$username
+    #sudo -k # clear sudo password cache
     }
 
-    try() { 
-        #tput sc
-        #printf "${reset}[       ] ${*}..."
-        printf "${*} ..."
-        if eval_result=$(eval "$@" 2>&1) ; then
-            #stty -echo; tput rc; tput cuf 1; stty echo
-            #printf "${success}success${reset}\n" 
-            [ "$verbose" ] && p "${eval_result}\n"
-        else
-            #stty -echo; tput rc; tput cuf 1; stty echo
-            printf "${failed}failed${reset}"
-            printf "${stderr}${eval_result}\n"
-            log "ERROR ${*} ${eval_result} ...failed\n"
-        fi
-    }
-# }}}
-
-# Intro {{{
-    welcome() {
-        h1 "Welcome to setup of $(head -n1 /etc/os-release | sed 's/PRETTY_NAME=//')"
-        p "  Kernel: $(uname -r)"
-        p "  Machine name:  $(uname -n)"
-        p "  Processor type: $(uname -p)\n"
-        read -p "  ${question}Continue(y/n)? ${answer}" yn
-        [ "$yn" = "n" ] && printf "\n${failed}Aborting.${reset}\n\n" && exit 1 || p
+apt_update_upgrade() {
+    eecho "apt update..."
+    apt update -y
+    eecho "apt upgrade..."
+    apt upgrade -y
     }
 
-    inputUserName() {
-        h1 'Create User'
-        read -p "${question}  Username: ${answer}" username; printf "${reset}"
+continue_as_user() {
+    eecho "Creating user directories..." 
+    USERHOME=/home/$username
+    USERCONFIG=$USERHOME/.config; mkdir -p $USERCONFIG
+    USERBIN=$USERHOME/.local/bin; mkdir -p $USERBIN
+    USERAPPIMAGES=$USERHOME/.local/appiamges; mkdir -p $USERAPPIMAGES
+    eecho "Changing user to $username..." 
+    su $username
+    cd $USERHOME
+    echo "Set password for $username:"; passwd
+}
+
+install_linuxbrew() {
+    eecho "Installing linuxbrew..."
+    eecho "Downloading and running Brew install script..."
+    # cause the script to run non-interactively by piping from echo
+    echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    #test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+    eecho "brew install hello..."
+    brew install hello
     }
 
-    inputUserPass() {
-        printf "${question}  Password: "; stty -echo; read pass1; stty echo; p
-        printf "${question}  Re-enter Password: "; stty -echo; read pass2; stty echo; p "\n"
-        if ! [ "$pass1" = "$pass2" ]; then
-            printf "  ${failed}Passwords do not match. Try again:${reset}\n\n"
-            unset pass1 pass2
-            inputUserPass
-        fi
+install_packages() {
+    progsfile="https://raw.githubusercontent.com/pcmariani/scripts/main/progs.csv"
+    curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+    while IFS=, read -r package method comment; do
+        case "$method" in
+            "brew")
+                eecho "brew install $package"
+                brew install "$package"
+                ;;
+            *)
+                eecho "apt install $package"
+                sudo apt install -y "$package"
+                ;;
+        esac
+    done < /tmp/progs.csv
     }
-# }}}
+
+install_neovim() {
+    cd $USERAPPIMAGES
+    eecho "Downloading neovim-nightly..."
+    curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
+    eecho "Extracting nvim.appimage..."
+    ./nvim.appimage --appimage-extract
+    NVIMHOME=$USERAPPIMAGES/nvim-nightly/squashfs-root/usr/bin/
+    chmod u+x $NVIMHOME/nvim
+    cd $USERBIN
+    eecho "Creating simlink in bin..."
+    ln -s $NVIMHOME/nvim nvim
+    cd $USERHOME
+    }
+
+install_dotfiles() {
+    dotfiles_setup_script="https://raw.githubusercontent.com/pcmariani/scripts/main/setup-dotfiles.sh"
+    curl -Ls "$dotfiles_setup_script" | sed '/^#/d' > /tmp/setup-dotfiles.sh
+    /bin/bash -c /tmp/setup-dotfiles.sh
+    }
+
+install_vimplug() {
+    eecho "Downloading vim-plug..."
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    eecho "Installing plugins..."
+    nvim --headless +PlugInstall +qall
+    }
+
+finalize() {
+    eecho "Exiting back to root..."
+    exit
+    eecho "Done Debian WSL setup"
+}
+
+#   <~-- - -~<o>~- - --~>   #
+
+main() {
+    colors
+    debian_prereqs
+    fix_locale
+    create_user
+    apt_update_upgrade
+    continue_as_user
+    install_linuxbrew
+    install_packages
+    install_neovim
+    install_dotfiles
+    install_vimplug
+    finalize
+    }
+
+main
+
+#   <~-- - -~<o>~- - --~>   #
+
+
+#clear
+#log_init
+#welcome
+#inputUserName
+#inputUserPass
+#h1 'Starting setup...'
+#addSources
+
+#reset="$(tput sgr0)$(tput setaf 253)" #grey
+#green="$(tput setaf 214)" #orange
+#question="$(tput setaf 81)" #darker cyan
+#answer="$(tput bold)$(tput setaf 255)" #white
+#success="$(tput setaf 2)" #green
+#failed="$(tput setaf 1)" #red
+#stderr="$(tput setaf 175)" #white
+
+## Script core {{{
+#log_init() {
+#    logfile=./install-debian-testing.log
+#    [ "$logging" ] && log "INFO Starting install..." > $logfile
+#}
+#
+#log() {
+#    [ "$logging" ] && printf "$(date +"%T") $@\n" >> $logfile
+#}
+#
+#h1() {
+#    printf "${green}$@${reset}\n\n${reset}"
+#}
+#
+#p() {
+#    printf "${reset}$@\n"
+#}
+#
+#try() { 
+#    #tput sc
+#    #printf "${reset}[       ] ${*}..."
+#    printf "${*} ..."
+#    if eval_result=$(eval "$@" 2>&1) ; then
+#        #stty -echo; tput rc; tput cuf 1; stty echo
+#        #printf "${success}success${reset}\n" 
+#        [ "$verbose" ] && p "${eval_result}\n"
+#    else
+#        #stty -echo; tput rc; tput cuf 1; stty echo
+#        printf "${failed}failed${reset}"
+#        printf "${stderr}${eval_result}\n"
+#        log "ERROR ${*} ${eval_result} ...failed\n"
+#    fi
+#}
+## }}}
+#
+## Intro {{{
+#welcome() {
+#    h1 "Welcome to setup of $(head -n1 /etc/os-release | sed 's/PRETTY_NAME=//')"
+#    p "  Kernel: $(uname -r)"
+#    p "  Machine name:  $(uname -n)"
+#    p "  Processor type: $(uname -p)\n"
+#    read -p "  ${question}Continue(y/n)? ${answer}" yn
+#    [ "$yn" = "n" ] && printf "\n${failed}Aborting.${reset}\n\n" && exit 1 || p
+#}
+#
+#inputUserName() {
+#    h1 'Create User'
+#    read -p "${question}  Username: ${answer}" username; printf "${reset}"
+#}
+#
+#inputUserPass() {
+#    printf "${question}  Password: "; stty -echo; read pass1; stty echo; p
+#    printf "${question}  Re-enter Password: "; stty -echo; read pass2; stty echo; p "\n"
+#    if ! [ "$pass1" = "$pass2" ]; then
+#        printf "  ${failed}Passwords do not match. Try again:${reset}\n\n"
+#        unset pass1 pass2
+#        inputUserPass
+#    fi
+#}
+## }}}
 
 #addSources() {
     #echo " \
@@ -88,56 +244,21 @@ verbose=
     #apt update
     #}
 
-
-debian_prereqs() {
-    umask 022
-    echo "Set root password:"; passwd
-    for package in readline-common dialog apt-utils build-essential \
-        sudo wget make cmake file git ; do
-        echo "--> apt install $package..."
-        apt install -y "$package"
-    done
-    }
-
-fix_locale() {
-    echo "--> Fixing locale..."
-    apt purge locales -y
-    apt install locales -y
-    echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
-    dpkg-reconfigure --frontend=noninteractive locales
-    update-locale LANG=en_US.UTF-8
-    }
-
-createUser() {
-    # $username and $defaultShell env variables set by wsl setup script
-    echo "--> Creating user $username..."
-    useradd --create-home --user-group --shell /bin/$defaultShell "$username"
-    #echo "$username:$pass1" | chpasswd
-    #unset pass1 pass2
-    echo "--> Adding $username to sudo group..."
-    usermod -aG sudo "$username"
-    echo "--> Adding $username to sudoers..."
-    echo "$username  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$username
-    USERHOME=/home/$username
-    USERCONFIG=$USERHOME/.config; mkdir -p $USERCONFIG
-    USERBIN=$USERHOME/.local/bin; mkdir -p $USERBIN
-    USERAPPIMAGES=$USERHOME/.local/appiamges; mkdir -p $USERAPPIMAGES
-    }
-
-install_starship() {
-    echo "--> Installing Starship..."
-    # or brew
-    cd 
-    sudo -u "$username" sudo \
-        curl -fsSL https://starship.rs/install.sh | bash
-    }
-
-apt_update_upgrade() {
-    echo "--> apt update..."
-    apt update -y
-    echo "--> apt upgrade..."
-    apt upgrade -y
-    }
+#install_starship() {
+#    eecho "Installing Starship..."
+#    # or brew
+#    sudo -u "$username" sudo \
+#        curl -fsSL https://starship.rs/install.sh | bash
+#    }
+#
+#install_q() {
+#    eecho "Installing q..."
+#    # or brew
+#    pushd $USERBIN
+#    wget -c https://github.com/harelba/q/releases/download/2.0.19/q-text-as-data_2.0.19-2_amd64.deb
+#    dpkg -i q-text-as-data_2.0.19-2_amd64.deb
+#    popd
+#}
 
 #install_todotxt() {
 #    TODO look at wsl Debian for correct paths. Will need symlink to Dropbox
@@ -149,101 +270,7 @@ apt_update_upgrade() {
 #        CONFIG_DIR=$USERCONFIG \
 #        INSTALL_DIR=$USERBIN \
 #        BASH_COMPLETION=/usr/share/bash-completion/completions
-    #}
+#}
 
-install_q() {
-    echo "--> Installing q..."
-    # or brew
-    cd $USERBIN
-    wget -c https://github.com/harelba/q/releases/download/2.0.19/q-text-as-data_2.0.19-2_amd64.deb
-    dpkg -i q-text-as-data_2.0.19-2_amd64.deb
-    }
-
-install_linuxbrew() {
-    echo "--> Installing linuxbrew..."
-    cd $USERHOME
-    echo "--> Downloading and running Brew install script..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-    echo "--> brew install hello..."
-    brew install hello
-    }
-
-installwith_apt() {
-    echo "--> apt install $1"
-    sudo -u "$username" sudo apt install -y "$1"
-    }
-
-installwith_brew() {
-    echo "--> brew install $1"
-    sudo -u "$username" brew install "$1"
-    }
-
-install_mainloop() {
-    progsfile="https://raw.githubusercontent.com/pcmariani/scripts/main/progs.csv"
-    curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
-    while IFS=, read -r program method comment; do
-        case "$method" in
-            "brew")
-                installwith_brew "$program" "$comment"
-                ;;
-            *)
-                installwith_apt "$program" "$comment" 
-                ;;
-        esac
-    done < /tmp/progs.csv
-    }
-
-install_neovim() {
-    echo "--> Installing Neovim nightly..."
-    mkdir -p $USERAPPIMAGES
-    cd $USERAPPIMAGES
-    echo "--> Downloading nvim.appimage..."
-    sudo -u $username sudo \
-        curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
-    echo "--> Extracting nvim.appimage..."
-    ./nvim.appimage --appimage-extract
-    NVIMHOME=$USERAPPIMAGES/nvim-nightly/squashfs-root/usr/bin/
-    chmod u+x $NVIMHOME/nvim
-    cd $USERBIN
-    ln -s $NVIMHOME/nvim nvim
-    }
-
-#neovim_vimplug() {
-    #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    #   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    #nvim --headless +PlugInstall +qall
-    #}
-
-#install_dotfiles() { ;}
-
-
-main() {
-    #clear
-    #colorscheme
-    #log_init
-    #welcome
-    #inputUserName
-    #inputUserPass
-
-    #h1 'Starting setup...'
-    #addSources
-    debian_prereqs
-    fix_locale
-    createUser
-    install_starship
-    apt_update_upgrade
-    install_q
-    install_linuxbrew
-    install_mainloop
-    install_neovim
-
-    h1 "\n...done"
-    }
-
-main
 
 # vim: fdm=indent fdls=1 fdn=1

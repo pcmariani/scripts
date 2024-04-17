c874d1ff7cc830a1e4cf6f93351c5dfb0d67b0a9
Pete Mariani <pcmariani@gmail.com>
2024-04-17 15:34:13 -0400

    more improved commit message

--- yadm-commit-daily.sh
+++ yadm-commit-daily.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 yadm add -u
-yedm commit -m \
-	"daily - \
-  modified: $(yadm status | grep '.*modified:' | sed -e 's/.*modified: *//' -e 's/.*\///' | tr '\n' '|' | sed -e 's/\|$/\n/g' -e 's/\|/, /g')\
-  ; \
-  new files: $(yadm status | grep '.*new file:' | sed -e 's/.*new file: *//' -e 's/.*\///' | tr '\n' '|' | sed -e 's/\|$/\n/g' -e 's/\|/, /g')"
+yadm commit -m \
+	"auto commit - \
+modified: $(yadm status | grep '.*modified:' | sed -e 's/.*modified: *//' -e 's/.*\///' | tr '\n' '|' | sed -e 's/\|$/\n/g' -e 's/\|/, /g'); \
+new files: $(yadm status | grep '.*new file:' | sed -e 's/.*new file: *//' -e 's/.*\///' | tr '\n' '|' | sed -e 's/\|$/\n/g' -e 's/\|/, /g')"
 yadm push origin master
